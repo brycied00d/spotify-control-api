92094405cc4a714b8526f11f65c6545881fcfa22
Bryce Chidester <bryce@cobryce.com>
2014-02-16 17:29:50 -0800

    Update CLI tool with test code to set volume and return now playing.

--- cli_control.py
+++ cli_control.py
@@ -1,10 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import dbus
 import sys
 
 if __name__ == '__main__':
-  session = dbus.SessionBus.get_session()
+  bus = dbus.SessionBus()
+  # Setup the interfaces required to fetch Now Playing
+  player = bus.get_object('com.spotify.qt', '/')
+  iface = dbus.Interface(player, 'org.freedesktop.MediaPlayer2')
+  info = iface.GetMetadata()
+  # Print Now Playing
+  print str(info['xesam:artist'][0]) + " - " + str(info['xesam:title'])
+
+  # Set volume - doesn't work
+  print iface.SetVolume(50)
+  print iface.Volume()
+
+  session = bus.get_session()
   spotify = session.get_object("org.mpris.MediaPlayer2.spotify",
                                "/org/mpris/MediaPlayer2")
   # Hopefuly only good commands are passed
